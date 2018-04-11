a772a9d0ddf6785f90eeaa5408f6c816a4f924d1
Hien Nguyen <mr.nguyenhien1994@gmail.com>
2018-04-11 18:45:01 +0700

    correct qemu_kernel_roofs_start.sh and qemu_uboot_start.sh

--- qemu_scripts/qemu_uboot_start.sh
+++ qemu_scripts/qemu_uboot_start.sh
@@ -2,6 +2,6 @@
 
 sudo qemu-system-x86_64 \
 -nographic \
--bios u-boot_v2018.05-rc1/u-boot.rom \
+-bios ./build/bin/u-boot.rom \
 -netdev tap,id=t0,ifname=vnet0,script=qemu_scripts/qemu-ifup.sh,downscript=no -device e1000,netdev=t0
 
