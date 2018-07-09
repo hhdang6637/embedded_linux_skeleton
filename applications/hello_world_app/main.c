85caf75a2298120bd9022620000f1f4f85e7bd64
Hien Nguyen <mr.nguyenhien1994@gmail.com>
2018-07-09 18:58:46 +0700

    update the hello_world app: use FastCGI

--- applications/hello_world_app/main.c
+++ applications/hello_world_app/main.c
@@ -1,8 +1,42 @@
+/* Compile with: gcc -Wall -lfcgi fastcgi.c -o fastcgi
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcgiapp.h>
 
+#define printfcgi(...) FCGX_FPrintF(request->out, __VA_ARGS__)
+#define get_param(KEY) FCGX_GetParam(KEY, request->envp)
 
-int main(int argc, char const *argv[])
-{
-    printf("Hello world\n");
-    return 0;
+void handle_request(FCGX_Request *request) {
+    printfcgi("Content-Type: text/html; charset=utf-8\r\n\r\n");
+    printfcgi(""
+            "<!DOCTYPE html>"
+            "<html>"
+            "<head>"
+            "<title>Welcome</title>"
+            "</head>"
+            "<body>"
+            "<h1>Hello World!</h1>"
+            "<p>This is an combination between Hiawatha and FastCGI\n</p>"
+            "</body>"
+            "</html>"
+            );
+    printfcgi("\n");
+}
+
+int main(void) {
+    int sock;
+    FCGX_Request request;
+
+    FCGX_Init();
+    sock = FCGX_OpenSocket(":2005", 5);
+    FCGX_InitRequest(&request, sock, 0);
+
+    while (FCGX_Accept_r(&request) >= 0) {
+        handle_request(&request);
+        FCGX_Finish_r(&request);
+    }
+
+    return EXIT_SUCCESS;
 }
