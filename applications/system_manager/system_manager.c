0a9051a855c9e4e42bf1cf5b600fd00fa7f07060
Dang Huynh <hhdang6637@gmail.com>
2018-07-21 20:59:59 +0700

    change software behavior based on MODEL

--- applications/system_manager/system_manager.c
+++ applications/system_manager/system_manager.c
@@ -8,7 +8,11 @@ void system_manager_init() {
     system("mkdir /tmp/hiawatha");
     system("hiawatha -c /etc/hiawatha");
     system("web_handler");
-
+#ifdef arm_vexpress_a9
+    if ((access("/dev/mmcblk0", F_OK)) != -1 && (access("/mnt", F_OK) != -1)) {
+        system("mount -t vfat /dev/mmcblk0 /mnt");
+    }
+#else
     if ((access("/dev/mmcblk0p2", F_OK)) != -1 && (access("/mnt", F_OK) != -1)) {
         system("mount -t vfat /dev/mmcblk0p2 /mnt");
     }
@@ -16,4 +20,5 @@ void system_manager_init() {
     if ((access("/dev/mmcblk0p3", F_OK)) != -1 && (access("/data", F_OK) != -1)) {
         system("mount -t ext4 /dev/mmcblk0p3 /data/");
     }
+#endif
 }
