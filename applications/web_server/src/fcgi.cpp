bc0181b5c20e1670d82ca2a7844fdcb4ee6a43e6
Hien Nguyen <mr.nguyenhien1994@gmail.com>
2018-07-24 23:02:16 +0700

    processing file upload: save in /tmp

--- applications/web_server/src/fcgi.cpp
+++ applications/web_server/src/fcgi.cpp
@@ -12,9 +12,13 @@
 
 #include <iostream>
 #include <fstream>
-#include "simplewebfactory.h"
+#include <algorithm>
 
+#include "simplewebfactory.h"
 #include "fcgi.h"
+#include "Parser.h"
+#include "Field.h"
+#include "Exception.h"
 
 static int fcgi_sock = -1;
 static FCGX_Request fcgi_request;
@@ -38,9 +42,63 @@ static void fcgi_init()
 #define printfcgi(...) FCGX_FPrintF(request->out, __VA_ARGS__)
 #define get_param(KEY) FCGX_GetParam(KEY, request->envp)
 
+static int process_data(const char *input, const char *contentType)
+{
+    MPFD::Parser *POSTParser;
+    try {
+        POSTParser = new MPFD::Parser();
+        POSTParser->SetTempDirForFileUpload("/tmp");
+        POSTParser->SetMaxCollectedDataLength(32*1024*1024);
+
+        POSTParser->SetContentType(contentType);
+
+        const int ReadBufferSize = strlen(input);
+
+        POSTParser->AcceptSomeData(input, ReadBufferSize);
+
+        // Now see what we have:
+        std::map<std::string,MPFD::Field *> fields=POSTParser->GetFieldsMap();
+
+        std::cout << "Have " << fields.size() << " fields\n\r";
+
+        std::map<std::string,MPFD::Field *>::iterator it;
+        for (it=fields.begin();it!=fields.end();it++) {
+            if (fields[it->first]->GetType()==MPFD::Field::TextType) {
+                std::cout<<"Got text field: ["<<it->first<<"], value: ["<< fields[it->first]->GetTextTypeContent() <<"]\n";
+            } else {
+                std::cout<<"Got file field: ["<<it->first<<"] Filename:["<<fields[it->first]->GetFileName()<<"] \n";
+                std::cout << fields[it->first]->GetTempFileName() << std::endl;
+            }
+        }
+    } catch (MPFD::Exception e) {
+        std::cout << "Parsing input error: " << e.GetError() << std::endl;
+        // FinishConnectionProcessing();
+        return 0;
+    }
+
+    return 1;
+}
+
+static std::string get_content_type(FCGX_Request *request)
+{
+    std::string contentType = FCGX_GetParam("CONTENT_TYPE", request->envp);
+
+    std::string retStr      = "multipart/form-data; ";
+
+    std::string boundary = contentType.substr(contentType.find("boundary"));
+
+    // rm -------------
+    boundary.erase(std::remove(boundary.begin(), boundary.end(), '-'), boundary.end());
+
+    retStr += boundary;
+
+    return retStr;
+}
+
 static void handle_firmware_upgrade(FCGX_Request *request)
 {
     const char *contentLength = FCGX_GetParam("CONTENT_LENGTH", request->envp);
+
     int content_len = 0;
 
     if (contentLength) {
@@ -52,11 +110,7 @@ static void handle_firmware_upgrade(FCGX_Request *request)
     for (int len = 0; len < content_len; len++) {
         int ch = FCGX_GetChar(request->in);
 
-        printf("CHAR: %c\n", ch);
-
         if (ch < 0) {
-            printf("Not enough bytes received on standard input, got %d, wanted ",
-                   content_len);
             content_len = len;
             return;
 
@@ -65,11 +119,7 @@ static void handle_firmware_upgrade(FCGX_Request *request)
         }
     }
 
-    std::ofstream out("/tmp/output.txt");
-
-    out << post_data;
-
-    out.close();
+    process_data(post_data.c_str(), get_content_type(request).c_str());
 }
 
 static void handle_request(FCGX_Request *request)
