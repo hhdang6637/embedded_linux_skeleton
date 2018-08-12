6e70bcca84e9cac64433083e4019e07f4759e0f0
Dang Huynh <hhdang6637@gmail.com>
2018-08-12 22:50:47 +0700

    support_pi_3_b: add brcmfmac43430-sdio firmware

--- build_cache/get_linux_pi_3_b_cache.sh
+++ build_cache/get_linux_pi_3_b_cache.sh
@@ -4,7 +4,10 @@ LINUX_CACHE=$(git ls-tree HEAD linux-4.14.22 | awk '{print "build_cache/"$3"_bui
 if [ -e $LINUX_CACHE ]; then
 	echo "found $LINUX_CACHE , reuse it"
 	tar -xvf $LINUX_CACHE
-	mv build/pi_b_plus/linux/* build/pi_3_b/linux
+	rm -rf build/pi_3_b/linux
+	mv build/pi_b_plus/linux build/pi_3_b/
+	rm -rf build/pi_3_b/linux_mod
+	mv build/pi_b_plus/linux_mod build/pi_3_b/
 	rm -rf build/pi_b_plus
 	exit 0;
 else
