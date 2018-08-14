410e82791df15cbf3dfc26a0a882a9533a58df3a
Dang Huynh <hhdang6637@gmail.com>
2018-08-14 22:02:51 +0700

    seperate linux for pi_3_b, enable SMP

--- build_cache/get_linux_pi_3_b_cache.sh
+++ build_cache/get_linux_pi_3_b_cache.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-LINUX_CACHE=$(git ls-tree HEAD linux-4.14.22 | awk '{print "build_cache/"$3"_build_pi_b_plus_linux.tar.gz"}')
+LINUX_CACHE=$(git ls-tree HEAD linux-4.14.22 | awk '{print "build_cache/"$3"_build_pi_3_b_linux.tar.gz"}')
 if [ -e $LINUX_CACHE ]; then
 	echo "found $LINUX_CACHE , reuse it"
 	tar -xvf $LINUX_CACHE
-	rm -rf build/pi_3_b/linux
-	mv build/pi_b_plus/linux build/pi_3_b/
-	rm -rf build/pi_3_b/linux_mod
-	mv build/pi_b_plus/linux_mod build/pi_3_b/
-	rm -rf build/pi_b_plus
 	exit 0;
 else
 	echo "not found $LINUX_CACHE"
