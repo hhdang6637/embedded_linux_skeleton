a3de894d9a1832f625624b69a4378ff4f0070e9f
quoctri1 <phqtri@gmail.com>
2018-08-24 13:28:04 +0700

    create action add user

--- applications/web_server/src/system_manager_js.cpp
+++ applications/web_server/src/system_manager_js.cpp
@@ -15,6 +15,15 @@
 #include "rpcMessageResourceHistory.h"
 #include "rpcMessageUsers.h"
 
+#include <fcgiapp.h>
+#include <syslog.h>
+
+#include "firmware_manager_js.h"
+#include "MPFDParser/Parser.h"
+#include "MPFDParser/Field.h"
+#include "MPFDParser/Exception.h"
+#include "user.h"
+
 #define TO_KBIT(a) ((a * 8 ) / 1000)
 
 typedef std::list<float> tx_rates_t;
@@ -153,12 +162,81 @@ std::string json_resource_usage_history(FCGX_Request *request)
     return ss_json.str();
 }
 
+
+//don't forget to delete
+// ========================================================================================================= //
+
+static int valid_user(std::string fullname, std::string user_name, std::string password, std::string repassword,
+                        std::string email)
+{
+    //compare password and repassword
+    if (password.compare(repassword) != 0) {
+        return 0;
+    }
+
+    return 1;
+}
+
+static int do_add_user(app::user &user)
+{
+    app::rpcUnixClient* rpcClient = app::rpcUnixClient::getInstance();
+    app::rpcMessageUsers msg;
+
+    if (rpcClient->doRpc(&msg) == false) {
+        syslog(LOG_ERR, "%s:%d - something went wrong: doRpc\n", __FUNCTION__, __LINE__);
+        return 0;
+    }
+
+    std::list<app::user> users = msg.getUsers();
+
+    for(auto &u : users) {
+        if (user.getName().compare(u.getName()) == 0) {
+            return 0;
+        }
+    }
+
+    msg.setUser(user);
+
+    if (rpcClient->doRpc(&msg) == false) {
+        syslog(LOG_ERR, "%s:%d - something went wrong: doRpc\n", __FUNCTION__, __LINE__);
+        return 0;
+    }
+
+    return 1;
+}
+
+static bool get_post_data(FCGX_Request *request, std::string &data)
+{
+    const char *contentLenStr = FCGX_GetParam("CONTENT_LENGTH", request->envp);
+    int         contentLength = 0;
+
+    if (contentLenStr) {
+        contentLength = strtol(contentLenStr, NULL, 10);
+    }
+
+    for (int len = 0; len < contentLength; len++) {
+        int ch = FCGX_GetChar(request->in);
+
+        if (ch < 0) {
+
+            syslog(LOG_ERR, "Failed to get file content\n");
+            return false;
+
+        } else {
+            data += ch;
+        }
+    }
+
+    return true;
+}
+
 std::string json_handle_users(FCGX_Request *request)
 {
     const char *method      = FCGX_GetParam("REQUEST_METHOD", request->envp);
-    std::ostringstream ss_json;
+    const char *contentType = FCGX_GetParam("CONTENT_TYPE", request->envp);
 
     if (method && (strcmp(method, "GET") == 0)) {
+        std::ostringstream ss_json;
 
         char filterUser[32];
         if (fcgi_form_varable_str(request, "user", filterUser, sizeof(filterUser)) <= 0) {
@@ -209,7 +287,51 @@ std::string json_handle_users(FCGX_Request *request)
         } else {
             syslog(LOG_ERR, "can't call rpc to get user list");
         }
+        return ss_json.str();
     }
 
-    return ss_json.str();
+    if (method && (strcmp(method, "POST") == 0) && contentType) {
+
+        std::string data;
+
+        if (get_post_data(request, data))
+        {
+            app::user user;
+            std::string fullname, user_name, password, repassword, email;
+            try
+            {
+                MPFD::Parser POSTParser;
+
+                POSTParser.SetContentType(contentType);
+
+                POSTParser.AcceptSomeData(data.c_str(), data.size());
+
+                fullname = POSTParser.GetField("fullname")->GetTextTypeContent();
+                user_name = POSTParser.GetField("user_name")->GetTextTypeContent();
+                password = POSTParser.GetField("password")->GetTextTypeContent();
+                repassword = POSTParser.GetField("repassword")->GetTextTypeContent();
+                email = POSTParser.GetField("email")->GetTextTypeContent();
+
+            } catch (MPFD::Exception &e) {
+                syslog(LOG_ERR, "%s\n", e.GetError().c_str());
+                return "failed";
+            }
+
+            if (valid_user(fullname, user_name, password, repassword, email))
+            {
+                user.setFullName(fullname.c_str());
+                user.setName(user_name.c_str());
+                user.setPassword(password.c_str());
+                user.setEmail(email.c_str());
+
+                if (do_add_user(user)) {
+                    return "succeeded";
+                }
+            }
+        }
+    }
+
+    syslog(LOG_ERR, "Failed to add user\n");
+
+    return "failed";
 }
