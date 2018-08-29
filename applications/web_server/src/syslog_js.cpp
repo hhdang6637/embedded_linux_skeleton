ecc61c29d43f457aa4cd63920eab1d385d4dadff
Dang Huynh <hhdang6637@gmail.com>
2018-08-29 19:56:57 +0700

    add jsonQuote, show syslog to web gui

--- applications/web_server/src/syslog_js.cpp
+++ applications/web_server/src/syslog_js.cpp
@@ -9,6 +9,46 @@
 #include <sstream>
 
 #include "fcgi.h"
+#include "simplewebfactory.h"
+
+static void jsonQuote(std::ostringstream &ss, std::string str)
+{
+    ss << '"';
+    for (char& c : str) {
+        switch (c)
+        {
+        case '\\':
+        case '"':
+            ss << '\\';
+            ss << c;
+            break;
+        case '/':
+            //                if (b == '<') {
+            ss << '\\';
+            //                }
+            ss << c;
+            break;
+        case '\b':
+            ss << "\\b";
+            break;
+        case '\t':
+            ss << "\\t";
+            break;
+        case '\n':
+            ss << "\\n";
+            break;
+        case '\f':
+            ss << "\\f";
+            break;
+        case '\r':
+            ss << "\\r";
+            break;
+        default:
+            ss << c;
+        }
+    }
+    ss << '"';
+}
 
 std::string json_handle_syslog(FCGX_Request *request)
 {
@@ -16,9 +56,12 @@ std::string json_handle_syslog(FCGX_Request *request)
     std::ostringstream ss_json;
 
     if (method && (strcmp(method, "GET") == 0)) {
-        ss_json << "{\"syslog\": \"";
-        ss_json << "aaaaaaaaaaaaaaaaaaaaaaaa";
-        ss_json << "\"}";
+        std::string syslog_str;
+        ss_json << "{\"syslog\": ";
+        if (simpleWebFactory::file_to_string("/tmp/messages", syslog_str)) {
+            jsonQuote(ss_json, syslog_str);
+        }
+        ss_json << "}";
     }
 
     return ss_json.str();
