ac739beaca55e3ba91d0c89ac9de9979fb42e070
Dang Huynh <hhdang6637@gmail.com>
2018-09-06 21:22:05 +0700

    change code format

--- applications/web_server/src/wifisetting_js.cpp
+++ applications/web_server/src/wifisetting_js.cpp
@@ -22,7 +22,9 @@ static std::string ssid, preshared_key;
 static int security_type, access_point;
 
 static inline std::string build_wifisetting_rsp_json(std::string status, std::string message = "") {
+
     std::ostringstream ss_json;
+
     ss_json << "{";
     ss_json << "\"status\": \"" << status <<"\",";
     ss_json << "\"message\": \""<< message <<"\"";
@@ -38,8 +40,9 @@ static inline bool validateSsid(const char* _ssid)
     int i = 0;
     int lengthSsid = 0;
 
-    if(_ssid == NULL)
+    if(_ssid == NULL) {
         return false;
+    }
 
     lengthSsid = strlen(_ssid);
 
@@ -47,23 +50,25 @@ static inline bool validateSsid(const char* _ssid)
         return false;
     }
 
-    for(; i < lengthSsid; i++)
-    {
+    for(; i < lengthSsid; i++) {
+
         c = (char*) strchr(specialKeyAllow, _ssid[i]);
 
-        if(c == NULL)
-        {
+        if(c == NULL) {
             //in range [0-9]
-            if(_ssid[i] >= '0' && _ssid[i] <= '9')
+            if(_ssid[i] >= '0' && _ssid[i] <= '9') {
                 continue;
+            }
 
             //in range [A-F]
-            if(_ssid[i] >= 'A' && _ssid[i] <= 'F')
+            if(_ssid[i] >= 'A' && _ssid[i] <= 'F') {
                 continue;
+            }
 
             //in range[a-f]
-            if(_ssid[i] >= 'a' && _ssid[i] <= 'f')
+            if(_ssid[i] >= 'a' && _ssid[i] <= 'f') {
                 continue;
+            }
 
             return false;
         }
@@ -77,41 +82,48 @@ static inline bool validatePresharedKey(const char* pwd)
     int i = 0;
     int lengthPwd = 0;
 
-    if(pwd == NULL)
-        return false;
-    lengthPwd = strlen(pwd);
-
-    if(lengthPwd < 8 || lengthPwd > 64)
-        return false;
-
-    for(i = 0; i < lengthPwd; i++)
-    {
-        //in range [0-9]
-        if(pwd[i] >= '0' && pwd[i] <= '9')
-            continue;
-
-        if(lengthPwd == 64)
-        {
-            //in range [A-F]
-            if(pwd[i] >= 'A' && pwd[i] <= 'F')
-                continue;
-
-            //in range[a-f]
-            if(pwd[i] >= 'a' && pwd[i] <= 'f')
-                continue;
-        }
-        else
-        {
-            //in range [A-Z]
-            if(pwd[i] >= 'A' && pwd[i] <= 'Z')
-                continue;
-
-            //in range[a-z]
-            if(pwd[i] >= 'a' && pwd[i] <= 'z')
-                continue;
-        }
+    if(pwd == NULL) {
         return false;
     }
+
+    lengthPwd = strlen(pwd);
+
+    if(lengthPwd < 8 || lengthPwd > 64) {
+        return false;
+    }
+
+    for(i = 0; i < lengthPwd; i++) {
+        //in range [0-9]
+        if(pwd[i] >= '0' && pwd[i] <= '9') {
+            continue;
+        }
+
+        if(lengthPwd == 64) {
+            //in range [A-F]
+            if(pwd[i] >= 'A' && pwd[i] <= 'F') {
+                continue;
+            }
+
+            //in range[a-f]
+            if(pwd[i] >= 'a' && pwd[i] <= 'f') {
+                continue;
+            }
+
+        } else {
+            //in range [A-Z]
+            if(pwd[i] >= 'A' && pwd[i] <= 'Z') {
+                continue;
+            }
+
+            //in range[a-z]
+            if(pwd[i] >= 'a' && pwd[i] <= 'z') {
+                continue;
+            }
+        }
+
+        return false;
+    }
+
     return true;
 }
 
@@ -127,8 +139,7 @@ std::string json_handle_wifisetting(FCGX_Request *request)
 
         std::string data;
 
-        if (get_post_data(request, data))
-        {
+        if (get_post_data(request, data)) {
             try
             {
                 MPFD::Parser POSTParser;
@@ -150,6 +161,8 @@ std::string json_handle_wifisetting(FCGX_Request *request)
             }
         }
     }
+
     status.assign("succeeded");
+
     return build_wifisetting_rsp_json(status);
 }
