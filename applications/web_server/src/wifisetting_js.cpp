853c7bb5cf70f85bd021d78d7e292b72a0073e74
HuuPhuc25 <thphucdh15pm@gmail.com>
2018-09-06 18:26:30 +0700

    add security types, validate ssid, validate preshared key

--- applications/web_server/src/wifisetting_js.cpp
+++ applications/web_server/src/wifisetting_js.cpp
@@ -18,6 +18,9 @@
 #include "fcgi.h"
 
 
+static std::string ssid, preshared_key;
+static int security_type, access_point;
+
 static inline std::string build_wifisetting_rsp_json(std::string status, std::string message = "") {
     std::ostringstream ss_json;
     ss_json << "{";
@@ -26,6 +29,90 @@ static inline std::string build_wifisetting_rsp_json(std::string status, std::st
     ss_json << "}";
 
     return ss_json.str();
+}
+
+static inline bool validateSsid(const char* _ssid)
+{
+    const char* specialKeyAllow = " .-_";
+    char *c = NULL;
+    int i = 0;
+    int lengthSsid = 0;
+
+    if(_ssid == NULL)
+        return false;
+
+    lengthSsid = strlen(_ssid);
+
+    if(lengthSsid > 32){
+        return false;
+    }
+
+    for(; i < lengthSsid; i++)
+    {
+        c = (char*) strchr(specialKeyAllow, _ssid[i]);
+
+        if(c == NULL)
+        {
+            //in range [0-9]
+            if(_ssid[i] >= '0' && _ssid[i] <= '9')
+                continue;
+
+            //in range [A-F]
+            if(_ssid[i] >= 'A' && _ssid[i] <= 'F')
+                continue;
+
+            //in range[a-f]
+            if(_ssid[i] >= 'a' && _ssid[i] <= 'f')
+                continue;
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static inline bool validatePresharedKey(const char* pwd)
+{
+    int i = 0;
+    int lengthPwd = 0;
+
+    if(pwd == NULL)
+        return false;
+    lengthPwd = strlen(pwd);
+
+    if(lengthPwd < 8 || lengthPwd > 64)
+        return false;
+
+    for(; i < lengthPwd; i++)
+    {
+        //in range [0-9]
+        if(pwd[i] >= '0' && pwd[i] <= '9')
+            continue;
+
+        if(lengthPwd == 64)
+        {
+            //in range [A-F]
+            if(pwd[i] >= 'A' && pwd[i] <= 'F')
+                continue;
+
+            //in range[a-f]
+            if(pwd[i] >= 'a' && pwd[i] <= 'f')
+                continue;
+        }
+        else
+        {
+            //in range [A-Z]
+            if(pwd[i] >= 'A' && pwd[i] <= 'Z')
+                continue;
+
+            //in range[a-z]
+            if(pwd[i] >= 'a' && pwd[i] <= 'z')
+                continue;
+        }
+        return false;
+    }
+    return true;
 }
 
 std::string json_handle_wifisetting(FCGX_Request *request)
@@ -42,8 +129,6 @@ std::string json_handle_wifisetting(FCGX_Request *request)
 
         if (get_post_data(request, data))
         {
-            std::string preshared_key, ssid, access_point;
-
             try
             {
                 MPFD::Parser POSTParser;
@@ -54,7 +139,10 @@ std::string json_handle_wifisetting(FCGX_Request *request)
 
                 preshared_key = POSTParser.GetField("preshared_key")->GetTextTypeContent();
                 ssid = POSTParser.GetField("ssid")->GetTextTypeContent();
-                access_point = POSTParser.GetField("access_point")->GetTextTypeContent();
+                access_point = atoi(POSTParser.GetField("access_point")->GetTextTypeContent().c_str());
+                security_type = atoi(POSTParser.GetField("security_type")->GetTextTypeContent().c_str());
+
+                printf("ssid: %s\n preshared_key: %s\n access_point: %d\n security_type:%d\n", ssid.c_str(), preshared_key.c_str(), access_point, security_type);
 
             } catch (MPFD::Exception &e) {
                 syslog(LOG_ERR, "%s\n", e.GetError().c_str());
