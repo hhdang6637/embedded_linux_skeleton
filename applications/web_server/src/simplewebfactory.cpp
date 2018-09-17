848cd84a2d0ed33eb9b98a775fabe55a4b89de0d
quoctri1 <phqtri@gmail.com>
2018-09-17 09:35:54 +0700

    create waiting page; not show nav bar in waiting page

--- applications/web_server/src/simplewebfactory.cpp
+++ applications/web_server/src/simplewebfactory.cpp
@@ -425,8 +425,13 @@ const char* simpleWebFactory::get_html_str(const char * url)
     ss_html << "<body>\n";
 
     if (it->first.compare("/pages/login") != 0) {
-        ss_html << this->html_navbar_str;
+
+        if (it->first.compare("/pages/waiting") != 0) {
+
+            ss_html << this->html_navbar_str;
+        }
     }
+
 
     // container begin
     ss_html << "<div class=\"container-fluid\"><div class=\"row\">\n";
