9344712034d499d50ba20ec3ca427ec629f0400a
quoctri1 <phqtri@gmail.com>
2018-10-16 18:51:04 +0700

    change boot.cmd to load firmware

--- configs/arm_vexpress_a9/scripts/boot.cmd
+++ configs/arm_vexpress_a9/scripts/boot.cmd
@@ -1,3 +1,2 @@
-firmwareload 0x71000000
-setenv bootargs console=ttyAMA0,115200 earlyprintk root=/dev/root rootwait panic=10 loglevel=4
-bootm 0x71000000
+ext4load mmc 0:1 0x44000000  /boot/firmware
+bootm 0x44000000
