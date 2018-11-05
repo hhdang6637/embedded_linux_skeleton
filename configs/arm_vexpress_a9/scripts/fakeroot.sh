6cab5bc9d35880a6263ff62d4c6beb24f354554f
quoctri1 <phqtri@gmail.com>
2018-11-05 19:55:33 +0700

    add configs arm_vexpress_a9

---
+++ configs/arm_vexpress_a9/scripts/fakeroot.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cd $BUILD_DIR
+mkdir -p rootfs_tmp
+cd rootfs_tmp
+cpio -i < $BIN_BUILD_DIR/rootfs.cpio
+mkdir -p lib/modules/4.14.22/kernel
+cp -r $LINUX_MOD_BUILD_DIR/lib .
+cp -r $SKELETON_ROOTFS_DIR/* .
+cp -r $ROOTFS_DIR/* .
+
+# remove /var/run
+if [ -e var/run ]; then
+    rm -rf var/run
+fi
+# add new symbol link /var/run -> /tmp
+cd var && ln -sf ../tmp run && cd ..
+
+# remove /root
+if [ -e root ]; then
+    rm -rf root
+fi
+# add new symbol link /root -> /tmp/root
+ln -sf tmp/root root
+rm -rf run; ln -sf tmp run
+ln -sf tmp home
+
+rm etc/passwd
+cd etc && ln -sf ../tmp/passwd passwd && cd ..
+
+# mkdir /boot
+mkdir boot
+
+find . -print | cpio -o -H newc > ../sdcard_boot/rootfs.cpio
+cd ..
+rm -rf rootfs_tmp
