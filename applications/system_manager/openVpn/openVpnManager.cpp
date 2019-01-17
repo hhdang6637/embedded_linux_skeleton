91511fe6ad8bd532f0ad9fd8dd3581cc8a05aa6d
HuuPhuc25 <thphucdh15pm@gmail.com>
2019-01-17 15:12:22 +0700

    Create openssl_init, openssl_req, openssl_gen_ca and openssl_sign methods in openVpnManager.cpp

--- applications/system_manager/openVpn/openVpnManager.cpp
+++ applications/system_manager/openVpn/openVpnManager.cpp
@@ -73,24 +73,89 @@ bool openVpnManager_openvpnCfg_set(app::openvpnCfg_t *openvpnCfg_ptr){
     return false;
 }
 
-static bool openssl_genrsa(const char *dst_key, int bitsize) {
-    // ex: openssl genrsa -des3 -out ca.key 4096
-    std::string command_genrsakey;
+static bool openssl_init()
+{
+    system("mkdir -p /tmp/myCA");
+    system("mkdir -p /tmp/myCA/certs");
+    system("mkdir -p /tmp/myCA/keys");
+    system("mkdir -p /tmp/myCA/reqs");
+    system("touch /tmp/myCA/index.txt");
+    system("echo 01 >> /tmp/myCA/serial");
+    return true;
+}
 
-    command_genrsakey += "openssl genrsa ";
-    command_genrsakey += dst_key;
-    command_genrsakey += " ";
-    command_genrsakey += std::to_string(bitsize);
+static bool openssl_req(const char *dst_key, const char* dst_csr, int days, int bitSize) {
+    // ex: openssl req -nodes -newkey rsa:1024 -keyout /tmp/myCA/keys/server.pem -days 365 -keyform PEM -out /tmp/myCA/reqs/server.pem -outform PEM 
+        //-subj "/C=VN/ST=HCM/L=HCM/O=Example Security/OU=IT Department/CN=example.com"
 
-    if(system(command_genrsakey.c_str()) == 0)
+    std::string command_gen_req;
+
+    command_gen_req += " openssl req  -newkey rsa:";
+    command_gen_req += std::to_string(bitSize);
+    command_gen_req += " -keyout ";
+    command_gen_req += dst_key;
+    command_gen_req += "  -days ";
+    command_gen_req += std::to_string(days);
+    command_gen_req += " -keyform PEM -out  ";
+    command_gen_req += dst_csr;
+    command_gen_req += " -outform PEM ";
+    command_gen_req += " -subj \"/C=VN/ST=HCM/L=HCM/O=Example Security/OU=IT Department/CN=example.com/emailAddress=server@gmail.com\" ";
+    printf("Command Gen REQ: %s\n", command_gen_req.c_str());
+    if(system(command_gen_req.c_str()) == 0)
     {
         return true;
+    }
+    return false;
+}
+
+static bool openssl_gen_ca(const char* ca_key, const char* ca_crt, int days, int bitsize)
+{
+    std::string command_gen_ca_key;
+
+    command_gen_ca_key += "openssl genrsa -out ";
+    command_gen_ca_key += ca_key;
+    command_gen_ca_key += " ";
+    command_gen_ca_key += std::to_string(bitsize);
+    printf("Command CA: %s\n", command_gen_ca_key.c_str());
+    if(system(command_gen_ca_key.c_str()) == 0)
+    {
+        std::string command_gen_ca_crt;
+        command_gen_ca_crt += "openssl req -config /tmp/myCA/CA.cnf -new -x509 -days ";
+        command_gen_ca_crt += std::to_string(days);
+        command_gen_ca_crt += " -key ";
+        command_gen_ca_crt += ca_key;
+        command_gen_ca_crt += " -out ";
+        command_gen_ca_crt += ca_crt;
+        command_gen_ca_crt += " -outform PEM ";
+        printf("Gen ca Certificate: %s\n", command_gen_ca_crt.c_str());
+        if(system(command_gen_ca_crt.c_str()) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     return false;
 }
 
-static bool openssl_req(const char *src_key, const char* ca_crt, int days) {
-    // ex: openssl req -new -x509 -days 365 -key ca.key -out ca.crt
+static bool openssl_sign(const char* src_csr, const char* dst_crt, int days)
+{
+    // ex: openssl ca -config configCA.cnf -in req/server.csr -out cert/server.crt
+    std::string command_sign;
+    command_sign += "yes | openssl ca -config /tmp/myCA/CA.cnf -in ";
+    command_sign += src_csr;
+    command_sign += " -days ";
+    command_sign += std::to_string(days);
+    command_sign += " -out ";
+    command_sign += dst_crt;
+    printf("Command sign: %s\n", command_sign.c_str());
+    if(system(command_sign.c_str()) == 0)
+    {
+        return true;
+    }
+
     return false;
 }
