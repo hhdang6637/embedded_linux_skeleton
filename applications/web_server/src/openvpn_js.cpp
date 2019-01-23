f309dbe264bb873c7dbf5f3da491135a8f2faf86
quoctri1 <phqtri@gmail.com>
2019-01-23 18:06:24 +0700

    ca and server cert web handler

--- applications/web_server/src/openvpn_js.cpp
+++ applications/web_server/src/openvpn_js.cpp
@@ -89,3 +89,60 @@ std::string json_handle_openvpn_cfg(FCGX_Request *request)
 
     return build_openvpn_rsp_json(status, "failed");
 }
+
+std::string json_handle_openvpn_cert(FCGX_Request *request)
+{
+    const char *method      = FCGX_GetParam("REQUEST_METHOD", request->envp);
+    const char *contentType = FCGX_GetParam("CONTENT_TYPE", request->envp);
+    std::string status      = "failed";
+
+    if (method && (strcmp(method, "POST") == 0) && contentType) {
+        // POST
+        std::string data;
+        std::string ca_name;
+        std::string server_name;
+
+        if (get_post_data(request, data)) {
+            try {
+
+                MPFD::Parser POSTParser;
+
+                POSTParser.SetContentType(contentType);
+                POSTParser.AcceptSomeData(data.c_str(), data.size());
+
+                ca_name = POSTParser.GetFieldText("ca");
+                server_name = POSTParser.GetFieldText("server");
+
+                status = "succeeded";
+
+                return build_openvpn_rsp_json(status, "see the syslog to get detail");
+
+            } catch (MPFD::Exception &e) {
+                syslog(LOG_ERR, "%s\n", e.GetError().c_str());
+                return build_openvpn_rsp_json(status, e.GetError().c_str());
+            }
+
+        } else {
+            syslog(LOG_ERR, "Failed to get data from browser\n");
+            return build_openvpn_rsp_json(status, "Failed to get data from browser");
+        }
+
+    } else if (method && (strcmp(method, "GET") == 0)) {
+        std::ostringstream ss_json;
+
+        ss_json << "{\"json_openvpn_cert\": {";
+
+        ss_json << "\"ca_name\":";
+        ss_json << "\"CA name\"";
+        ss_json << ", ";
+
+        ss_json << "\"server_name\":";
+        ss_json << "\"SERVER name\"";
+
+        ss_json << "}}";
+
+        return ss_json.str();
+    }
+
+    return build_openvpn_rsp_json(status, "failed");
+}
--- applications/web_server/src/openvpn_js.h
+++ applications/web_server/src/openvpn_js.h
@@ -4,5 +4,6 @@
 #include <fcgiapp.h>
 
 std::string json_handle_openvpn_cfg(FCGX_Request *request);
+std::string json_handle_openvpn_cert(FCGX_Request *request);
 
 #endif /* _OPENVPN_JS_H_ */
