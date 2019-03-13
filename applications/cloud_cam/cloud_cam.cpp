335907c5118a19820e8a14d1bb0bb39089a7641b
Dang Huynh <hhdang6637@gmail.com>
2019-03-13 16:50:53 +0700

    simple cloudcam service

---
+++ applications/cloud_cam/cloud_cam.cpp
@@ -0,0 +1,215 @@
+#include <string.h>
+#include <syslog.h>
+#include <math.h>
+#include <arpa/inet.h>
+#include "simpleTimerSync.h"
+#include "netlink_socket.h"
+#include "utilities.h"
+
+typedef struct
+{
+    char macs[6];
+    char mac_vendor[64];
+} mac_addr;
+
+typedef struct
+{
+    unsigned char ips[4];
+    mac_addr      mac;
+} host_info;
+
+static std::list<std::string> list_subnet_new;
+static std::list<host_info> list_addr;
+
+static void update_network_addresses(void);
+static void split_subnet(const char* subnet, std::list<std::string> &list_addr);
+static void scan_camera_rtsp(const char* subnet, std::list<host_info> &list_found_ips);
+
+static void start_nmap_thread(void);
+static void *nmap_thread_work(void *context);
+
+void cloud_cam_service_loop()
+{
+    fd_set read_fds;
+
+    app::simpleTimerSync *timer = app::simpleTimerSync::getInstance();
+    timer->init(1000);
+    timer->addCallback(1000*60*60, update_network_addresses);
+    timer->start();
+
+    std::list<int> listReadFd;
+    listReadFd.push_back(timer->getTimterFd());
+
+    sleep(60);
+
+    update_network_addresses();
+
+    while (1) {
+        int maxfd = build_fd_sets(&read_fds, listReadFd);
+
+        int activity = select(maxfd + 1, &read_fds, NULL, NULL, NULL);
+
+        switch (activity)
+        {
+        case -1:
+            if (errno != EINTR) {
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 0:
+            // TODO
+            continue;
+
+        default:
+        {
+            if (FD_ISSET(timer->getTimterFd(), &read_fds)) {
+                timer->do_schedule();
+            }
+        }
+        }
+    }
+}
+
+static void update_network_addresses()
+{
+    FILE* f_stream;
+    char line[512];
+    int len;
+
+    if (list_subnet_new.size() > 0) {
+        syslog(LOG_INFO, "the list new subnet still available");
+        return;
+    }
+
+    f_stream = popen("ip -o -f inet addr show | awk '/scope global/ {print $4}'", "r");
+
+    if (f_stream) {
+        while (fgets(line, sizeof(line), f_stream) != NULL) {
+            len = strlen(line);
+            if (line[len - 1] == '\n') {
+                line[len - 1] = '\0';
+            }
+            split_subnet(line, list_subnet_new);
+        }
+        pclose(f_stream);
+    }
+
+    if (list_subnet_new.size() > 0) {
+        start_nmap_thread();
+    }
+}
+
+void start_nmap_thread(void)
+{
+    pthread_t nmapThread;
+    pthread_attr_t  thread_attr;
+    pthread_attr_init (&thread_attr);
+    pthread_create(&nmapThread, &thread_attr, nmap_thread_work, NULL);
+    pthread_attr_destroy(&thread_attr);
+}
+
+void *nmap_thread_work(void *context)
+{
+    syslog(LOG_NOTICE, "nmap_thread_work start");
+
+    list_addr.clear();
+
+    while(list_subnet_new.size() > 0)
+    {
+        std::string subnet = list_subnet_new.back();
+        scan_camera_rtsp(subnet.c_str(), list_addr);
+        list_subnet_new.pop_back();
+    }
+
+    syslog(LOG_NOTICE, "nmap found:");
+    for (std::list<host_info>::iterator i = list_addr.begin(); i != list_addr.end(); ++i) {
+        syslog(LOG_NOTICE, "%hhu.%hhu.%hhu.%hhu",
+            i->ips[0], i->ips[1], i->ips[2],
+            i->ips[3]);
+    }
+
+    syslog(LOG_NOTICE, "nmap_thread_work done");
+    return NULL;
+}
+
+void scan_camera_rtsp(const char* subnet, std::list<host_info> &list_found_ips)
+{
+    char nmap_cmd[256];
+    char buff[512];
+    FILE* nmap_out_stream;
+    host_info host;
+
+    memset(&host, 0, sizeof(host));
+
+    snprintf(nmap_cmd, 256, "nmap -n -T5 %s -p T:554 --open -oN -", subnet);
+
+    syslog(LOG_NOTICE, "nmap: %s", nmap_cmd);
+
+    nmap_out_stream = popen(nmap_cmd, "r");
+
+    if (nmap_out_stream) {
+        while (fgets(buff, sizeof(buff), nmap_out_stream) != NULL) {
+            unsigned char ips[4];
+            char macs[6];
+            char mac_vendor[64];
+
+            if (sscanf(buff, "Nmap scan report for %hhu.%hhu.%hhu.%hhu", &ips[0], &ips[1], &ips[2], &ips[3]) == 4) {
+
+                if (host.ips[0] != 0) {
+                    // store the old host info
+                    list_found_ips.push_back(host);
+                }
+
+                memset(&host, 0, sizeof(host));
+                memcpy(host.ips, ips, 4);
+            } else if (sscanf(buff, "MAC Address: %hhx:%hhx:%hhx:%hhx:%hhx:%hhx %[^\t\n]", &macs[0], &macs[1], &macs[2],
+                    &macs[3], &macs[4], &macs[5], mac_vendor) == 7) {
+                memcpy(host.mac.macs, macs, 6);
+                memcpy(host.mac.mac_vendor, mac_vendor, 64);
+            }
+        }
+
+        if (host.ips[0] != 0) {
+            // store the old host info
+            list_found_ips.push_back(host);
+        }
+
+        pclose(nmap_out_stream);
+    }
+}
+
+void split_subnet(const char* subnet, std::list<std::string> &list_addr)
+{
+    unsigned char ips[4];
+    int subnet_len;
+
+    if (sscanf(subnet, "%hhu.%hhu.%hhu.%hhu/%d", &ips[0], &ips[1], &ips[2], &ips[3], &subnet_len) != 5)
+    {
+        syslog(LOG_WARNING, "subnet %s is invalid", subnet);
+        return;
+    }
+
+    if (subnet_len >= 32 || subnet_len < 16)
+    {
+        syslog(LOG_WARNING, "subnet %s is invalid", subnet);
+        return;
+    }
+
+    if (subnet_len >= 24)
+    {
+        list_addr.push_back(subnet);
+        return;
+    }
+
+    int bits = 24 - subnet_len;
+
+    unsigned char ip2 = (ips[2] >> bits) << bits;
+
+    int i;
+    char newsubnet[64];
+    for (i = 0; i < pow(2, bits); ++i) {
+        snprintf(newsubnet, sizeof(newsubnet), "%d.%d.%d.0/24",
+                ips[0], ips[1], ip2 + i);
+        list_addr.push_back(newsubnet);
+    }
+}
--- applications/cloud_cam/main.cpp
+++ applications/cloud_cam/main.cpp
@@ -1,81 +1,67 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdbool.h>
 #include <string.h>
-#include <iostream>
-#include <list>
+#include <unistd.h>
+#include <stdlib.h>
+#include <syslog.h>
 
-#include "opencv2/core/core.hpp"
-#include "opencv2/gpu/gpu.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/contrib/contrib.hpp"
+#include "utilities.h"
+#include "backtrace_signal.h"
 
-#include "cloud_cam.h"
+#define SERVICE_NAME    "cloud_cam"
+#define PID_FILE_NAME   "/var/run/cloud_cam.pid"
 
-extern void scan_camera_rtsp(const char* subnet, std::list<host_info> &list_found_ips);
-extern void find_list_accesable_camera(std::list<host_info> &list_found_ips, std::list<host_info> &list_accesable_camera);
+typedef struct {
+    bool daemon;
+} global_settings;
 
-static void send_rstp_to_jpg_buff(host_info &host);
+static global_settings settings = {
+        .daemon = true
+};
 
-extern void mqtt_init();
-extern void MQTTClient_loop();
-extern void mqtt_publish_topic(const char *topic, unsigned char *buff, size_t size);
+static void show_help(const char *app_name) {
+    printf("Usage: %s [options]\n", app_name);
+    printf("         -d: don't fork to the background.\n");
+}
 
-#define JPG_STREAM "jpg_stream"
+void parse_arguments(int argc, char const *argv[]) {
+    int i = 0;
 
-std::list<host_info> list_found_ips;
-std::list<host_info> list_accesable_camera;
+    while (++i < argc) {
+        if (strcmp(argv[i], "-d") == 0) {
+            settings.daemon = false;
+        } else if (strcmp(argv[i], "-h") == 0) {
+            show_help(argv[0]);
+            exit(EXIT_SUCCESS);
+        } else {
+            fprintf(stderr, "Unknown option. Use '-h' for help.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+extern void cloud_cam_service_loop(void);
 
 int main(int argc, char const *argv[])
 {
-    std::string subnet = argv[1];
+    openlog(SERVICE_NAME, 0, LOG_USER);
 
-    scan_camera_rtsp(subnet.c_str(), list_found_ips);
-    find_list_accesable_camera(list_found_ips, list_accesable_camera);
+    backtrace_init_signal();
 
-    mqtt_init();
+    parse_arguments(argc, argv);
 
-    MQTTClient_loop();
+    if (settings.daemon) {
+        if (daemon(1, 1)) {
+            syslog(LOG_ERR, "fork is not scuccess");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    write_pid(PID_FILE_NAME, getpid());
+
+    syslog(LOG_NOTICE,"%s init complete\n", SERVICE_NAME);
+
+    cloud_cam_service_loop();
 
     return 0;
-}
-
-void send_rstp_to_jpg_buff(host_info &host) {
-    char buff[64];
-
-    snprintf(buff, sizeof(buff), "rtsp://%hhu.%hhu.%hhu.%hhu/",
-        host.ips[0], host.ips[1], host.ips[2],
-        host.ips[3]);
-
-    cv::VideoCapture reader(buff);
-    cv::Mat image;
-
-    int n = 0;
-
-    printf("collecting frame from %s\n", buff);
-
-    if (reader.isOpened()) {
-        while(1) {
-            if (reader.read(image)) {
-
-                n++;
-
-                printf("\rread frame #%05d\t", n);
-                std::vector<uchar> buff;
-
-                if (cv::imencode(".jpg", image, buff)) {
-                    // hangs
-                    // Mat im2 = imdecode(buff,CV_LOAD_IMAGE_ANYDEPTH);
-                    printf("buff size is %d\t", buff.size());
-                    fflush(stdout);
-                    mqtt_publish_topic(JPG_STREAM, &buff[0], buff.size());
-                }
-
-            } else {
-                fprintf(stderr,"cannot read frame #%d\n", n);
-                break;
-            }
-        }
-
-        reader.release();
-    }
 }
